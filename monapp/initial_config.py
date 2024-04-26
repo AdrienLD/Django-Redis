50141827f00c5ab6a046b9751831793726c10c96
AdrienLD <adrien.laduca@icloud.com>
2024-04-26 15:58:12 +0200

    add manage students

---
+++ monapp/initial_config.py
@@ -0,0 +1,76 @@
+import json
+import redis
+
+def get_redis_client():
+    r = redis.Redis
+    r = redis.Redis(host='localhost', port=6379)
+    return r
+
+def create_course(course_id, titre, prof):
+    r = get_redis_client()
+    course_data = {
+        'id': course_id,
+        'titre': titre,
+        'prof': prof,
+        'etudiants': json.dumps([])
+    }
+    course_key = f"cours:{course_id}"
+    for key, value in course_data.items():
+        r.hset(course_key, key, value)
+
+def create_student(student_id, nom, prenom, cours):
+    r = get_redis_client()
+    student_data = {
+        'id': student_id,
+        'nom': nom,
+        'prenom': prenom,
+        'cours': json.dumps(cours)
+    }
+    student_key = f"etudiant:{student_id}"
+    for key, value in student_data.items():
+        r.hset(student_key, key, value)
+
+def create_teacher(teacher_id, nom, prenom, cours):
+    r = get_redis_client()
+    teacher_data = {
+        'id': teacher_id,
+        'nom': nom,
+        'prenom': prenom,
+        'cours': json.dumps(cours)
+    }
+    teacher_key = f"prof:{teacher_id}"
+    for key, value in teacher_data.items():
+        r.hset(teacher_key, key, value)
+
+
+
+def initialize(request):
+    create_course('101', 'Programmation Web', 'M. Dupont')
+    create_course('102', 'Base de données', 'M. Durand')
+    create_course('103', 'Réseaux', 'M. Martin')
+    create_course('104', 'Intelligence Artificielle', 'Mme. Leblanc')
+    create_course('105', 'Systèmes d\'exploitation', 'M. Lambert')
+    create_course('106', 'Algorithmique', 'Mme. Dupuis')
+    create_course('107', 'Sécurité informatique', 'M. Lefebvre')
+    create_course('108', 'Analyse de données', 'Mme. Lemoine')
+
+    create_student('201', 'Dupont', 'Jean', ['101', '102', '103'])
+    create_student('202', 'Durand', 'Paul', ['101', '104', '105'])
+    create_student('203', 'Martin', 'Jacques', ['101', '102', '106'])
+    create_student('204', 'Leblanc', 'Marie', ['101', '102', '107'])
+    create_student('205', 'Lambert', 'Sophie', ['101', '102', '108'])
+    create_student('206', 'Dupuis', 'Alice', ['101', '103', '104'])
+    create_student('207', 'Lefebvre', 'Julie', ['101', '105', '106'])
+    create_student('208', 'Lemoine', 'Marine', ['101', '107', '108'])
+
+
+    create_teacher('301', 'Dupont', 'M.', ['101'])
+    create_teacher('302', 'Durand', 'M.', ['102'])
+    create_teacher('303', 'Martin', 'M.', ['103'])
+    create_teacher('304', 'Leblanc', 'Mme.', ['104'])
+    create_teacher('305', 'Lambert', 'M.', ['105'])
+    create_teacher('306', 'Dupuis', 'Mme.', ['106'])
+    create_teacher('307', 'Lefebvre', 'M.', ['107'])
+    create_teacher('308', 'Lemoine', 'Mme.', ['108'])
+
+    return redirect('manage_courses')
--- monapp/views.py
+++ monapp/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+import redis
+import json
+from monapp.initial_config import get_redis_client
 
-# Create your views here.
+def select_student(request):
+    r = get_redis_client()
+    liste_students = r.keys('etudiant:*')
+    student_keys = [key.decode('utf-8') for key in liste_students]
+    students = []
+    for key in student_keys:
+        student_data = r.hgetall(key)
+        student_info = {k.decode('utf-8'): v.decode('utf-8') for k, v in student_data.items()}
+        students.append(student_info)
+
+    return render(request, 'etudiants.html', {'students': students})
+
+def manage_student(request, id_etudiant):
+    r = get_redis_client()
+    student_key = f"etudiant:{id_etudiant}"
+    student_data = r.hgetall(student_key)
+    student = {k.decode('utf-8'): v.decode('utf-8') for k, v in student_data.items()}
+    cours_liste = r.keys('cours:*')
+    student_keys = [key.decode('utf-8') for key in cours_liste]
+    cours_details = []
+    for key in student_keys:
+        cours_data = r.hgetall(key)
+        cours_info = {k.decode('utf-8'): v.decode('utf-8') for k, v in cours_data.items()}
+        cours_details.append(cours_info)
+    return render(request, 'manage.html', {'student': student, 'cours_details': cours_details})
+
+def delete_cours(request, id_etudiant, id_cours):
+    r = get_redis_client()
+    student_key = f"etudiant:{id_etudiant}"
+    student_data = r.hgetall(student_key)
+    student = {k.decode('utf-8'): v.decode('utf-8') for k, v in student_data.items()}
+    cours_ids = json.loads(student['cours'])
+    cours_ids.remove(str(id_cours))
+    r.hset(student_key, 'cours', json.dumps(cours_ids))
+
+    
+    return redirect('manage_student', id_etudiant=id_etudiant)
+
+def add_cours(request, id_etudiant, id_cours):
+    r = get_redis_client()
+    student_key = f"etudiant:{id_etudiant}"
+    student_data = r.hgetall(student_key)
+    student = {k.decode('utf-8'): v.decode('utf-8') for k, v in student_data.items()}
+    cours_ids = json.loads(student['cours'])
+    cours_ids.append(str(id_cours))
+    r.hset(student_key, 'cours', json.dumps(cours_ids))
+    
+    return redirect('manage_student', id_etudiant=id_etudiant)
