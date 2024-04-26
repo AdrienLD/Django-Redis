cf7998c7796e5801ecd0fa933f0cf0e045f7a4f7
AdrienLD <adrien.laduca@icloud.com>
2024-04-26 16:29:54 +0200

    Ajouter la gestion des étudiants

--- monapp/views.py
+++ monapp/views.py
@@ -13,7 +13,15 @@ def select_student(request):
         student_info = {k.decode('utf-8'): v.decode('utf-8') for k, v in student_data.items()}
         students.append(student_info)
 
-    return render(request, 'etudiants.html', {'students': students})
+    liste_profs = r.keys('prof:*')
+    prof_keys = [key.decode('utf-8') for key in liste_profs]
+    profs = []
+    for key in prof_keys:
+        prof_data = r.hgetall(key)
+        prof_info = {k.decode('utf-8'): v.decode('utf-8') for k, v in prof_data.items()}
+        profs.append(prof_info)
+
+    return render(request, 'etudiants.html', {'students': students, 'profs': profs})
 
 def manage_student(request, id_etudiant):
     r = get_redis_client()
@@ -27,7 +35,7 @@ def manage_student(request, id_etudiant):
         cours_data = r.hgetall(key)
         cours_info = {k.decode('utf-8'): v.decode('utf-8') for k, v in cours_data.items()}
         cours_details.append(cours_info)
-    return render(request, 'manage.html', {'student': student, 'cours_details': cours_details})
+    return render(request, 'manage_student.html', {'student': student, 'cours_details': cours_details})
 
 def delete_cours(request, id_etudiant, id_cours):
     r = get_redis_client()
@@ -51,3 +59,23 @@ def add_cours(request, id_etudiant, id_cours):
     r.hset(student_key, 'cours', json.dumps(cours_ids))
     
     return redirect('manage_student', id_etudiant=id_etudiant)
+
+def subscribe_cours(request, id_etudiant, id_cours):
+    r = get_redis_client()
+    canal_nouvelles = id_cours
+    pubsub = r.pubsub()
+    pubsub.subscribe(canal_nouvelles)
+
+    return redirect('manage_student', id_etudiant=id_etudiant)
+
+def update(request):
+    r = get_redis_client()
+    canal_nouvelles = id_cours
+    nouvelles_messages = []
+
+    messages = r.pubsub(num=1).parse_response()
+    for message in messages:
+        if isinstance(message, dict) and 'data' in message:
+            nouvelles_messages.append(message['data'].decode())
+
+    return JsonResponse({'nouvelles_messages': nouvelles_messages})
--- monprojet/urls.py
+++ monprojet/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('manage/select', monapp.views.select_student, name='select_student'),
     path('manage/<int:id_etudiant>', monapp.views.manage_student, name='manage_student'),
     path('delete_cours/<int:id_etudiant>/<int:id_cours>', monapp.views.delete_cours, name='delete_cours'),
-    path('add_cours/<int:id_etudiant>/<int:id_cours>', monapp.views.add_cours, name='add_cours')
+    path('add_cours/<int:id_etudiant>/<int:id_cours>', monapp.views.add_cours, name='add_cours'),
+    path('subscribe_cours/<int:id_etudiant>/<int:id_cours>', monapp.views.subscribe_cours, name='subscribe_cours'),
+    path('update', monapp.views.update, name='update')
 ]
